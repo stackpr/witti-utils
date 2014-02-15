814a220810355142fd265fedabf97e5e189c5b40
Greg Payne <greg.payne@gmail.com>
2014-02-15 13:41:53 -0600

    #2. Basic test suite configuration and initial csv tests.

---
+++ src/ArgValidate/Tests/GetNumericCsvTest.php
@@ -0,0 +1,41 @@
+<?php
+namespace Witti\Utils\ArgValidate\Tests;
+use Witti\Utils\ArgValidate;
+class GetNumericCsvTest extends \PHPUnit_Framework_TestCase {
+  public function getValidCsv() {
+    return array(
+      array(
+        "1,2,3,4",
+        TRUE,
+        1,
+        "1,2,3,4",
+      ),
+    );
+  }
+
+  public function getInvalidCsv() {
+    return array(
+      array(
+        "-1",
+        TRUE,
+        1
+      ),
+    );
+  }
+
+  /**
+   * @expectedException InvalidArgumentException
+   * @dataProvider getInvalidCsv
+   */
+  public function testInvalidCsv($csv, $positive, $decimals) {
+    ArgValidate::getNumericCsv($csv, $positive, $decimals);
+  }
+
+  /**
+   * @dataProvider getValidCsv
+   */
+  public function testValidCsv($csv, $positive, $decimals, $new_csv) {
+    $test = ArgValidate::getNumericCsv($csv, $positive, $decimals);
+    $this->assertEquals($new_csv, $test);
+  }
+}
